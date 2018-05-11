cc07c5ce7b5ffefa724f0b903af558989b49e147
Curtis Fulton <Curtis@mexbris.com>
2018-05-11 15:57:20 +1000

    First commit

---
+++ RegionAssetEditor/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace RegionAssetEditor
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Property Changed Event. This should never need to be manually called
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Helper function to force update a property
+        /// </summary>
+        /// <param name="propertyName">Name of the property that has been changed</param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RegionAssetEditor/ViewModel/HierarchyNodeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace RegionAssetEditor
+{
+    class HierarchyNodeViewModel
+    {
+        #region Private Fields
+
+
+
+        #endregion  
+
+        #region Public Properties
+
+        /// <summary>
+        /// Collection of all the children on this node
+        /// </summary>
+        public ObservableCollection<HierarchyNodeViewModel> AllChildren { get; private set; }
+
+        /// <summary>
+        /// Collection of the children that should be displayed
+        /// </summary>
+        public ObservableCollection<HierarchyNodeViewModel> DisplayChildren { get; set; }
+
+        /// <summary>
+        /// True if the node is expanded. False if it is not.
+        /// </summary>
+        public bool IsExpanded { get; set; }
+
+        /// <summary>
+        /// If the node has children or not. By Default this will be true, and will only be checked when all children is set.
+        /// </summary>
+        public bool HasChildren { get; set; }
+
+        #endregion 
+    }
+}
